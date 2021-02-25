b60f1256cc9344165e97128457ce1e7f63713ced
Graemehedges <graeme.hedges@gmail.com>
2021-02-25 13:04:15 +0000

    All user stories met

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -18,14 +18,14 @@ describe('Thermostat', function() {
     it('increases the temperature by 1', function(){
       thermostat.up();
       expect(thermostat.temperature()).toEqual(21);
-    })
-  })
+    });
+  });
 
   describe('.down', function() {
     it('decrases the temperature by 1', function(){
       thermostat.down();
       expect(thermostat.temperature()).toEqual(19);
-    })
+    });
 
     it('cannot decrease the temperature below 10', function(){
       thermostat = new Thermostat(thermostat.MINIMUM_TEMPERATURE);
@@ -34,5 +34,57 @@ describe('Thermostat', function() {
       expect(thermostat.temperature()).toEqual(thermostat.MINIMUM_TEMPERATURE);
     }); 
   });
+
+  describe('.powerSave', function(){
+    it('A new thermostat has power saving on by default', function(){
+      expect(thermostat.powerSavingMode).toEqual(true);
+    });
+
+    it('A new thermostat has a maximum temperature of 25', function() {
+      expect(thermostat.maximumTemperature).toEqual(25);
+    });
+
+    it('Power Saving Mode can be toggled off and maximum temp to be 32', function(){
+      thermostat.powerSave();
+      expect(thermostat.powerSavingMode).toEqual(false);
+      expect(thermostat.maximumTemperature).toEqual(32);
+    });
+
+    it('Power Saving Mode can be toggled on and maximum temp to be 25', function(){
+      thermostat.powerSave();
+      expect(thermostat.powerSavingMode).toEqual(false);
+      expect(thermostat.maximumTemperature).toEqual(32);
+      thermostat.powerSave();
+      expect(thermostat.powerSavingMode).toEqual(true);
+      expect(thermostat.maximumTemperature).toEqual(25);    
+    });
+  });
+
+  describe('.reset',function(){
+    it('Resets the temperature to 20', function(){
+      thermostat = new Thermostat(25);
+      thermostat.reset();
+      expect(thermostat.temp).toEqual(20);
+    });
+  });
+
+  describe('.energyUsage', function(){
+    it('Returns low-usage when temp is below 18', function(){
+      thermostat = new Thermostat(17);
+      expect(thermostat.energyUsage()).toEqual('low-usage');
+    });
+
+    it('Returns medium-usage when temp equal to or less than 25', function(){
+      expect(thermostat.energyUsage()).toEqual('medium-usage');
+    });
+
+    it('Returns high-usage when temp is above 25', function(){
+      thermostat.powerSave();
+      for (var i = 0; i < 6; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.energyUsage()).toEqual('high-usage');
+    });
+  })
 });
   
--- src/thermostat.js
+++ src/thermostat.js
@@ -4,6 +4,7 @@ class Thermostat {
   constructor(temp = 20) {
     this.temp = temp
     this.powerSavingMode = true
+    this.maximumTemperature = 25
     this.MINIMUM_TEMPERATURE = 10
   };
   
@@ -17,9 +18,35 @@ class Thermostat {
 
   down() {
     if (this.temp === this.MINIMUM_TEMPERATURE) {
-      this.temp = 10;
+      return;
     } else {
     this.temp -= 1;
     }
   };
+
+  powerSave() {
+    if (this.powerSavingMode === true) {
+      this.powerSavingMode = false;
+      this.maximumTemperature = 32;
+      console.log("Power Saving mode off") 
+    } else {
+      this.powerSavingMode = true;
+      this.maximumTemperature = 25;
+      console.log("Power Saving mode on") 
+    }
+  }
+
+  reset() {
+    this.temp = 20;
+  }
+
+  energyUsage() {
+    if (this.temp < 18) {
+      return 'low-usage';
+    } else if (this.temp <= 25) {
+      return 'medium-usage';
+    } else {
+      return 'high-usage';
+    }
+  }
 };
